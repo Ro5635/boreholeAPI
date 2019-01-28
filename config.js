bb4f2e0118ce89dd0c868d2e4c5d013312125293
Robert Curran <ro5635@gmail.com>
2019-01-28 20:45:06 +0000

    Created single config file that provides the config values for the  application based on the current envirnment NODE_ENV state.
     Removed old implementation with config npm package and associated
     files
    
     Removed npm config package from package
    
     On branch configurationTest
     Changes to be committed:
    	modified:   Models/boreholeModel.js
    	new file:   config.js
    	deleted:    config/dev.json
    	deleted:    config/prod.json
    	deleted:    config/test.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   template.yml

---
+++ config.js
@@ -0,0 +1,38 @@
+/**
+ * Master configuration file
+ *
+ * Parameters in defaultConfig can be overridden in the environment specific declarations
+ */
+
+const env = process.env.NODE_ENV;
+
+const defaultConfig = {
+    AWS_API_CONFIG: {}
+
+};
+
+const dev = {
+    BOREHOLES_TABLE_NAME: "boreholesTable",
+    AWS_API_CONFIG: {region: "local", endpoint: 'http://localhost:8000'}
+
+};
+
+const test = {
+    BOREHOLES_TABLE_NAME: "boreholesTable",
+    AWS_API_CONFIG: {region: "local", endpoint: 'http://localhost:8000'}
+
+};
+
+const prod = {
+    BOREHOLES_TABLE_NAME: process.env.BOREHOLE_TABLE_NAME
+};
+
+const config = {
+    dev,
+    test,
+    prod
+};
+
+const currentConfig = Object.assign(defaultConfig, config[env]);
+
+module.exports = currentConfig;
